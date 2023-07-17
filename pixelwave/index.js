8de04199d1319593e52d5af1f64eacf7f4578900
tapfer-abdullah <tapfer.abdullah@gmail.com>
2023-07-17 12:37:09 +0600

    added aos

--- pixelwave/index.js
+++ pixelwave/index.js
@@ -1,3 +1,16 @@
+
+var typed = new Typed('#typeit1', {
+  strings: ["PixelWave Spectra"],
+  typeSpeed: 50,
+});
+var typed = new Typed('#typeit2', {
+  strings: ["Unleashing Digital Brilliance"],
+  typeSpeed: 50,
+  startDelay: 2000
+});
+
+
+
 const button = document.getElementById("button"); 
 const buttonPressed = (e) => {
   e.target.classList.toggle("active");
